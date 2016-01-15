c10ce05b672717e0776537bceaf5a776f23e76f9
robins35 <caiden@brivity.com>
2016-01-14 20:51:44 -0800

    Added a clone script to easily clone vim plugins

---
+++ .vim/bundle/clone_plugins.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+git clone https://github.com/scrooloose/nerdtree.git
+git clone git://github.com/tpope/vim-bundler.git
+git clone git://github.com/tpope/vim-endwise.git
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+git clone git://github.com/tpope/vim-rails.git
+git clone git://github.com/tpope/vim-surround.git
+git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/kchmck/vim-coffee-script.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/tpope/vim-projectionist.git
+git clone git://github.com/tpope/vim-rake.git
